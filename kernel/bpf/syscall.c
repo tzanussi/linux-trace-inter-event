78bb0059c3b8304a8d124b55feebc780fb3e0500
David Howells <dhowells@redhat.com>
2018-02-27 10:04:55 +0000

    bpf: Restrict kernel image access functions when the kernel is locked down
    There are some bpf functions can be used to read kernel memory:
    bpf_probe_read, bpf_probe_write_user and bpf_trace_printk.  These allow
    private keys in kernel memory (e.g. the hibernation image signing key) to
    be read by an eBPF program and kernel memory to be altered without
    restriction.
    
    Completely prohibit the use of BPF when the kernel is locked down.
    
    Suggested-by: Alexei Starovoitov <alexei.starovoitov@gmail.com>
    Signed-off-by: David Howells <dhowells@redhat.com>
    cc: netdev@vger.kernel.org
    cc: Chun-Yi Lee <jlee@suse.com>
    cc: Alexei Starovoitov <alexei.starovoitov@gmail.com>

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -1848,6 +1848,9 @@ SYSCALL_DEFINE3(bpf, int, cmd, union bpf_attr __user *, uattr, unsigned int, siz
 	if (!capable(CAP_SYS_ADMIN) && sysctl_unprivileged_bpf_disabled)
 		return -EPERM;
 
+	if (kernel_is_locked_down("BPF"))
+		return -EPERM;
+
 	err = check_uarg_tail_zero(uattr, sizeof(attr), size);
 	if (err)
 		return err;
